e3bea46794ebbc3241dd4709226d87b44ed18faa
ashoklamaid@gmail.com <ashoklama@Ashoks-MacBook-Pro.local>
2023-04-06 16:59:18 -0600

    first commit

---
+++ src/main/java/com/as/CardSystem/CardSystemApplication.java
@@ -0,0 +1,13 @@
+package com.as.CardSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/as/CardSystem/CardSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.CardSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
